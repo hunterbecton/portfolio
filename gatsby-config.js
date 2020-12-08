71a0c8151d3a7c6649b992be5e3292e4fee1744e
hunterbecton <hunter@hunterbecton.com>
2020-12-08 13:18:24 -0500

    Added sitemap and robot txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,10 @@ module.exports = {
     title: `Hunter Becton`,
     description: `Hunter Becton | Full Stack Developer and YouTuber in Atlanta, GA.`,
     author: `Hunter Becton`,
+    siteUrl: `https://hunterbecton.com`,
   },
   plugins: [
+    `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
@@ -24,6 +26,21 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://hunterbecton.com",
+        sitemap: "https://hunterbecton.com/sitemap.xml",
+        env: {
+          development: {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+          },
+          production: {
+            policy: [{ userAgent: "*", allow: "/" }],
+          },
+        },
       },
     },
   ],
